0cc2fae7f015ad97b701b9b01684fc4efa14b6ea
Zachary Soja <zachary3003@gmail.com>
2022-11-21 02:40:28 +0100

    feature: save workouts and read them from file

--- app/src/main/java/com/example/gymapp/WorkoutsFragment.kt
+++ app/src/main/java/com/example/gymapp/WorkoutsFragment.kt
@@ -7,18 +7,34 @@ import android.widget.LinearLayout
 import android.widget.Space
 import com.example.gymapp.databinding.FragmentWorkoutsBinding
 import com.google.android.material.button.MaterialButton
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
+import com.example.gymapp.models.Workout
+import java.io.File
+import java.lang.Exception
+
 
 class WorkoutsFragment : Fragment() {
 
     private var _binding: FragmentWorkoutsBinding? = null
+    private var _workouts: MutableList<Workout> = mutableListOf()
 
     private val binding get() = _binding!!
+    private val workouts get() = _workouts
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentWorkoutsBinding.inflate(inflater, container, false)
+
+        val path = context?.filesDir
+        try{
+            _workouts = Json.decodeFromString(File(path, "workouts.json").readText())
+        } catch (ex: Exception){
+            println(ex.message)
+        }
 
         return binding.root
     }
@@ -30,6 +46,10 @@ class WorkoutsFragment : Fragment() {
             val txt = binding.editAddWorkout.text.toString()
 
             if(txt == ""){
+                return@setOnClickListener
+            }
+
+            if(workouts.any { it.name == txt }){
                 return@setOnClickListener
             }
 
@@ -52,6 +72,36 @@ class WorkoutsFragment : Fragment() {
             binding.workoutList.addView(btn)
             binding.workoutList.addView(space)
             binding.editAddWorkout.setText("")
+
+            workouts.add(Workout(txt))
+
+            val path = context?.filesDir
+            try{
+                File(path, "workouts.json").writeText(Json.encodeToString(workouts))
+            } catch (ex: Exception){
+                println(ex.message)
+            }
+        }
+
+        for (wrk in workouts){
+            val btn = MaterialButton(ContextThemeWrapper(activity, R.style.MenuButton))
+            btn.layoutParams = LinearLayout.LayoutParams(
+                resources.getDimension(R.dimen.btn_width).toInt(),
+                resources.getDimension(R.dimen.btn_height).toInt()
+            )
+            (btn.layoutParams as LinearLayout.LayoutParams).apply{
+                gravity = Gravity.CENTER
+            }
+            btn.text = wrk.name
+
+            val space = Space(activity)
+            space.layoutParams = LinearLayout.LayoutParams(
+                0,
+                resources.getDimension(R.dimen.btn_spacing).toInt()
+            )
+
+            binding.workoutList.addView(btn)
+            binding.workoutList.addView(space)
         }
     }
 }
---
+++ app/src/main/java/com/example/gymapp/models/Workout.kt
@@ -0,0 +1,6 @@
+package com.example.gymapp.models
+
+import kotlinx.serialization.*
+
+@Serializable
+data class Workout(val name: String)
